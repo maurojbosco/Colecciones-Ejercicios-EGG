4c8386ea54023fa73d4db6f4abded342ad6047b1
Mauro Jelvez Bosco <mauro_jelvez@hotmail.com>
2022-08-18 11:36:38 -0300

    Ejercicio 3 terminado

---
+++ Ejercicio 3- Alumno/src/Entidad/Alumno.java
@@ -0,0 +1,42 @@
+package Entidad;
+
+import java.util.ArrayList;
+
+
+public class Alumno {
+    // Atributos
+    private String nombre;
+    private ArrayList<Integer> notas;
+    // Constructores
+
+    public Alumno(String nombre, ArrayList<Integer> notas) {
+        this.nombre = nombre;
+        this.notas = notas;
+    }
+
+    public Alumno() {
+    }
+
+    // Getters y Setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Integer> getNotas() {
+        return notas;
+    }
+
+    public void setNotas(ArrayList<Integer> notas) {
+        this.notas = notas;
+    }
+    //To String
+    @Override
+    public String toString() {
+        return "Alumno [nombre=" + nombre + ", notas=" + notas + "]";
+    }
+
+}
--- Ejercicio 3- Alumno/src/Servicio/AlumnoService.java
+++ Ejercicio 3- Alumno/src/Servicio/AlumnoService.java
@@ -1,5 +1,54 @@
 package Servicio;
 
+import Entidad.Alumno;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class AlumnoService {
-    
+
+    private ArrayList<Alumno> alumnos = new ArrayList();
+    private Scanner sn = new Scanner(System.in);
+
+    // Método que mediante un bucle crea un objeto Alumno. Pide toda la información
+    // al usuario y ese Alumno se guarda en una lista de tipo Alumno y le pregunta
+    // al usuario si quiere crear otro Alumno o no.
+    public ArrayList<Alumno> crearAlumnos() {
+
+        do {
+            Alumno alumno = new Alumno();
+            ArrayList<Integer> notas = new ArrayList();
+            System.out.println("Ingrese el nombre del alumno");
+            alumno.setNombre(sn.next());
+            System.out.println("Ingrese la nota 1 del alumno");
+            notas.add(0, sn.nextInt());
+            System.out.println("Ingrese la nota 2 del alumno");
+            notas.add(1, sn.nextInt());
+            System.out.println("Ingrese la nota 3 del alumno");
+            notas.add(2, sn.nextInt());
+            alumno.setNotas(notas);
+            alumnos.add(alumno);
+            System.out.println("Introduzca cualquier caracter para crear otro alumno o 's' para salir");
+        } while (!sn.next().equals("s"));
+        return alumnos;
+    }
+
+    // Método que le pide al usuario ingresar el nombre del alumno que quiere
+    // calcular su nota final y luego lo busca en la lista de Alumnos. Si está en la
+    // lista, usa la lista notas para calcular el promedio final de alumno.
+    public double notaFinal() {
+        System.out.println("Ingrese el nombre de alumno a calcular la nota final");
+        String alumnom = sn.next();
+        ArrayList<Integer> notas = new ArrayList();
+        double prom = 0;
+        double sum = 0;
+        for (Alumno alumno : alumnos) {
+            if (alumno.getNombre().equals(alumnom)) {
+                notas = alumno.getNotas();
+            }
+            sum = notas.stream().mapToInt(Integer::intValue).sum();
+            prom = sum / 3;
+        }
+        return prom;
+    }
+
 }
