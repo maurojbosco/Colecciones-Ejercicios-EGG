8ba48c82c8d50699e079f86050e0dfd65c105f8b
Mauro Jelvez Bosco <mauro_jelvez@hotmail.com>
2022-08-19 21:31:27 -0300

    ejercicio 6 terminado

--- Ejercicio 6- Tienda/src/App.java
+++ Ejercicio 6- Tienda/src/App.java
@@ -5,11 +5,44 @@ Estas las realizaremos en el servicio. Como, introducir un elemento, modificar s
 eliminar un producto y mostrar los productos que tenemos con su precio (Utilizar 
 Hashmap). El HashMap tendrá de llave el nombre del producto y de valor el precio. 
 Realizar un menú para lograr todas las acciones previamente mencionadas. */
+import servicio.ProductosService;
+import java.util.Scanner;
+
 public class App {
+
+    public static Scanner sn = new Scanner(System.in);
+    
     /**
      * @param args
      */
     public static void main(String[] args) {
-        
+        ProductosService proser = new ProductosService();
+        int opcion;
+        do {
+        System.out.println("Ingrese una opciíon:");
+        System.out.println("1. Introducir un nuevo producto");
+        System.out.println("2. Modificar el precio de un producto");
+        System.out.println("3. Eliminar un producto");
+        System.out.println("4. Mostrar todos los productos con su precio");
+        System.out.println("5. Salir");
+        opcion = sn.nextInt();
+        switch (opcion) {
+            case 1:
+            proser.introducirUnProducto();
+                
+                break;
+                case 2:
+                proser.modificarPrecio();
+                break;
+                case 3:
+                proser.eliminarProducto();
+                break;
+                case 4:
+                proser.mostrarProductos();
+                break;
+            default:
+                break;
+        }
+        } while (opcion!=5);
     }
 }
--- Ejercicio 6- Tienda/src/servicio/ProductosService.java
+++ Ejercicio 6- Tienda/src/servicio/ProductosService.java
@@ -1,5 +1,61 @@
 package servicio;
 
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.Map;
+
 public class ProductosService {
-    
+    private Scanner sn;
+    private HashMap<String, Integer> productos;
+
+    // Constructor
+    public ProductosService() {
+        this.sn = new Scanner(System.in);
+        this.productos = new HashMap<>();
+    }
+
+    // Método que le pide por teclado al usuario un producto y su respectivo precio
+    // para introducirlo al HashMap
+    public void introducirUnProducto() {
+        System.out.println("Ingrese el nombre del producto a almacenar");
+        String key = sn.next();
+        System.out.println("Ingrese el precio del producto a almacenar");
+        int value = sn.nextInt();
+        productos.put(key, value);
+    }
+
+    // Método que le pide al usuario por teclado el nombre de un producto para
+    // modificar su precio, luego le solicita el nuevo precio del producto
+    public void modificarPrecio() {
+        System.out.println("Ingrese el producto a actualizar su precio");
+        String producto = sn.next();
+        System.out.println("Ingrese el nuevo precio del producto");
+        int precio = sn.nextInt();
+        productos.replace(producto, precio);
+
+    }
+
+    // Método que le solicita al usuario el nombre de un producto para luego
+    // eliminarlo del HashMap
+    public void eliminarProducto() {
+        System.out.println("Ingrese el producto a eliminar");
+        String producto = sn.next();
+        productos.remove(producto);
+
+    }
+
+    // Método que muestra todos los productos almacenados en el HashMap con su
+    // respectivo precio
+    public void mostrarProductos() {
+        for (Map.Entry<String, Integer> entry : productos.entrySet()) {
+            System.out.println("producto: " + entry.getKey() + ", precio: " + entry.getValue());
+        }
+    }
+
+    // To String
+    @Override
+    public String toString() {
+        return "ProductosService [productos=" + productos + "]";
+    }
+
 }
